c7fc3ff600892ba9b20c6a7b4a82bf2cb17c8c22
Dmitry <dmitry.gr.web@gmail.com>
2024-05-04 03:32:39 +0300

    change config

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,3 +1,3 @@
-export const socketUrl = 'wss://194.61.52.136:8081/'
+export const socketUrl = 'ws://194.61.52.136:8081/'
 // export const socketUrl = 'ws://192.168.0.102:8081/'
 // export const socketUrl = 'ws://192.168.1.124:8081/'
